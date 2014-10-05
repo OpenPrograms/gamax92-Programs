8f224d2dbbd41c453a6d3e3b61f97daed7d19920
gamax92 <gamax92@aol.com>
2014-10-05 11:27:21 -0600

    ocwe
    Add kill, distr, tp
    Check for pos1/pos2 in replace
    Move existing pos1 and pos2 checks to the previous elseif

--- ocwe.lua
+++ ocwe.lua
@@ -4,7 +4,7 @@ if not component.isAvailable("debug") then
 	return
 end
 
-local player
+local player, username
 io.stdout:write("Input player name: ")
 while true do
 	player = io.read()
@@ -14,6 +14,7 @@ while true do
 		io.stdout:write("Input player name: ")
 	else
 		print("Bound OCWE to " .. player)
+		username = player
 		player = component.debug.getPlayer(player)
 		break
 	end
@@ -89,16 +90,14 @@ while true do
 			print("Invalid argument, Expected: set block")
 		elseif not id or not damage then
 			print("Invalid block code")
+		elseif not info.pos1 then
+			print("Please set pos1")
+		elseif not info.pos2 then
+			print("Please set pos2")
 		else
-			if not info.pos1 then
-				print("Please set pos1")
-			elseif not info.pos2 then
-				print("Please set pos2")
-			else
-				local size = (math.abs(info.pos1[1]-info.pos2[1])+1) * (math.abs(info.pos1[2]-info.pos2[2])+1) * (math.abs(info.pos1[3]-info.pos2[3])+1)
-				print("Setting " .. size .. " block" .. (size == 1 and "" or "s"))
-				world.setBlocks(info.pos1[1],info.pos1[2],info.pos1[3],info.pos2[1],info.pos2[2],info.pos2[3],id,damage)
-			end
+			local size = (math.abs(info.pos1[1]-info.pos2[1])+1) * (math.abs(info.pos1[2]-info.pos2[2])+1) * (math.abs(info.pos1[3]-info.pos2[3])+1)
+			print("Setting " .. size .. " block" .. (size == 1 and "" or "s"))
+			world.setBlocks(info.pos1[1],info.pos1[2],info.pos1[3],info.pos2[1],info.pos2[2],info.pos2[3],id,damage)
 		end
 	elseif parse[1] == "replace" then
 		local fid, fdamage = decodeBlock(parse[2])
@@ -109,6 +108,10 @@ while true do
 			print("Invalid from-block code")
 		elseif not tid or not tdamage then
 			print("Invalid to-block code")
+		elseif not info.pos1 then
+			print("Please set pos1")
+		elseif not info.pos2 then
+			print("Please set pos2")
 		else
 			local blocks = 0
 			
@@ -130,24 +133,85 @@ while true do
 			end
 			print("Replaced " .. blocks .. " block" .. (blocks == 1 and "" or "s"))
 		end
+	elseif parse[1] == "distr" then
+		if not info.pos1 then
+			print("Please set pos1")
+		elseif not info.pos2 then
+			print("Please set pos2")
+		else
+			local size = (math.abs(info.pos1[1]-info.pos2[1])+1) * (math.abs(info.pos1[2]-info.pos2[2])+1) * (math.abs(info.pos1[3]-info.pos2[3])+1)
+		
+			local x1,x2 = math.min(info.pos1[1],info.pos2[1]),math.max(info.pos1[1],info.pos2[1])
+			local y1,y2 = math.min(info.pos1[2],info.pos2[2]),math.max(info.pos1[2],info.pos2[2])
+			local z1,z2 = math.min(info.pos1[3],info.pos2[3]),math.max(info.pos1[3],info.pos2[3])
+
+			local stats = {}
+			
+			for y = y1,y2 do
+				for x = x1,x2 do
+					for z = z1,z2 do
+						local cid = world.getBlockId(x,y,z)
+						local cdamage = world.getMetadata(x,y,z)
+						local id = cid .. ":" .. cdamage
+						if stats[id] == nil then
+							stats[id] = 0
+						end
+						stats[id] = stats[id] + 1
+					end
+				end
+			end
+			local stats2 = {}
+			for k,v in pairs(stats) do	
+				stats2[#stats2 + 1] = {k,v}
+			end
+			table.sort(stats2,function(a,b) return a[2] < b[2] end)
+			for i = 1,#stats2 do
+				print(string.format("%-8d%s",stats2[i][2],stats2[i][1]))
+			end
+		end
 	elseif parse[1] == "walls" then
 		local id, damage = decodeBlock(parse[2])
 		if parse[2] == nil then
 			print("Invalid argument, Expected: walls block")
 		elseif not id or not damage then
 			print("Invalid block code")
+		elseif not info.pos1 then
+			print("Please set pos1")
+		elseif not info.pos2 then
+			print("Please set pos2")
 		else
-			if not info.pos1 then
-				print("Please set pos1")
-			elseif not info.pos2 then
-				print("Please set pos2")
+			local size = "NaN" -- >_> why is this confusing.
+			print("Setting " .. size .. " block" .. (size == 1 and "" or "s"))
+			world.setBlocks(info.pos1[1],info.pos1[2],info.pos1[3],info.pos1[1],info.pos2[2],info.pos2[3],id,damage)
+			world.setBlocks(info.pos2[1],info.pos1[2],info.pos1[3],info.pos2[1],info.pos2[2],info.pos2[3],id,damage)
+			world.setBlocks(info.pos1[1],info.pos1[2],info.pos1[3],info.pos2[1],info.pos2[2],info.pos1[3],id,damage)
+			world.setBlocks(info.pos1[1],info.pos1[2],info.pos2[3],info.pos2[1],info.pos2[2],info.pos2[3],id,damage)
+		end
+	elseif parse[1] == "tp" then
+		if parse[2] == nil then
+			print("Invalid argument, Expected: tp player")
+		else
+			local top = component.debug.getPlayer(parse[2])
+			local health,err = top.getHealth()
+			if not health or err == "player is offline" then
+				print("No such player")
 			else
-				local size = "NaN" -- >_> why is this confusing.
-				print("Setting " .. size .. " block" .. (size == 1 and "" or "s"))
-				world.setBlocks(info.pos1[1],info.pos1[2],info.pos1[3],info.pos1[1],info.pos2[2],info.pos2[3],id,damage)
-				world.setBlocks(info.pos2[1],info.pos1[2],info.pos1[3],info.pos2[1],info.pos2[2],info.pos2[3],id,damage)
-				world.setBlocks(info.pos1[1],info.pos1[2],info.pos1[3],info.pos2[1],info.pos2[2],info.pos1[3],id,damage)
-				world.setBlocks(info.pos1[1],info.pos1[2],info.pos2[3],info.pos2[1],info.pos2[2],info.pos2[3],id,damage)
+				print("Teleporting " .. username .. " to " .. parse[2])
+				tX,tY,tZ = top.getPosition()
+				player.setPosition(tX,tY,tZ)
+			end
+		end
+	elseif parse[1] == "kill" then
+		if parse[2] == nil then
+			print("Invalid argument, Expected: kill player")
+		else
+			local top = component.debug.getPlayer(parse[2])
+			local health,err = top.getHealth()
+			if not health or err == "player is offline" then
+				print("No such player")
+			else
+				print("Killing " .. parse[2])
+				top.setHealth(-1)
 			end
 		end
 	elseif parse[1] == "quit" then
